e7dea7cfcad52cdb3b613492fcb90c610a47c70e
Tanmay <wadhwa.tanmay@gmail.com>
2018-02-17 00:55:08 -0800

    cosmetic changes

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -1,6 +1,7 @@
 import { Blog } from '../app/models/blog';
 
 export const BLOGS: Blog[] = [
-  { title: 'First', body: 'something goes in here' },
-  { title: 'Second', body: 'Lorem Ipsum' },
+  { title: 'Test', body: 'Lorem Ipsum' },
+  { title: 'Test', body: 'Lorem Ipsum' },
+  { title: 'Test', body: 'Lorem Ipsum' },
 ];
